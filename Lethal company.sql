823f97b7b6d12a312baacab03f94bc95a63f134d
Fernando2407-rgb <osantillan1245@gmail.com>
2026-03-02 12:26:45 -0600

    Add files via upload

---
+++ Lethal company.sql
@@ -0,0 +1,34 @@
+-- Crear base de datos y seleccionar
+CREATE DATABASE IF NOT EXISTS ClinicPlus;
+USE ClinicPlus;
+
+-- Tabla Pacientes
+DROP TABLE IF EXISTS Pacientes;
+
+CREATE TABLE Pacientes(
+  idPaciente INT AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(50) NOT NULL,
+  edad INT CHECK (edad >= 0),
+  diagnostico VARCHAR(100)
+);
+
+-- Datos de ejemplo
+INSERT INTO Pacientes (nombre, edad, diagnostico) VALUES
+('Ana López', 29, 'Chequeo general'),
+('Bruno Díaz', 41, 'Hipertensión'),
+('Carla Pérez', 35, 'Alergia estacional');
+
+--  VALIDACIÓN MÁS EFICIENTE (por PRIMARY KEY)
+SELECT EXISTS (
+    SELECT 1
+    FROM Pacientes
+    WHERE idPaciente = 1
+    LIMIT 1
+) AS Existe;
+
+SELECT EXISTS (
+    SELECT 1
+    FROM Pacientes
+    WHERE nombre = 'NOMBRE_A_BUSCAR'
+    LIMIT 1
+) AS Existe;
